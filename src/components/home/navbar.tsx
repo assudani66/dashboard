817b33ce07f8a8c7e5188c32082e2c07ea9cec3c
Piyush Assudani <63794264+assudani66@users.noreply.github.com>
2023-08-07 23:15:08 +0530

    nav bar

--- src/components/home/navbar.tsx
+++ src/components/home/navbar.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Icons } from "./icons";
 
 const Navbar = () => {
   return (
-    <nav>
-      <section>
-        <p>Thomas Brown</p>
-        <p>Developer</p>
+    <nav className="flex justify-between p-2">
+      <section className="flex items-center">
+        <div className="flex items-center">
+          <Icons iconName="search" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type to search"
+          className="focus:outline-none"
+        />
+      </section>
+      <section className="flex space-x-4 items-center">
+        <Icons iconName="settings" />
+        <Icons iconName="messages" />
+        <Icons iconName="notification" />
+        <div className="flex flex-col items-center">
+          <p>Thomas Brown</p>
+          <p>Developer</p>
+        </div>
+        <img
+          className="w-16"
+          alt="avatar"
+          src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s176-c-k-c0x00ffffff-no-rj"
+        />
       </section>
     </nav>
   );
